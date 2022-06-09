2c35dcb96a6691ca635900b48bddf19f45c9545d
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2022-06-09 11:13:01 -0700

    Make top github repos install the same (#13)
    regardless of running in 'git' or 'user' mode -- that is, scheduled or
    on-demand mode.

--- run-build.ts
+++ run-build.ts
@@ -5,7 +5,7 @@ if (!process.send) process.exit(1);
 
 process.send('ready');
 
-process.on('message', ({repoDir, tscPath, testType, skipLibCheck}) => {
-    ge.buildAndGetErrors(repoDir, tscPath, testType, skipLibCheck)
+process.on('message', ({repoDir, tscPath, topGithubRepos, skipLibCheck}) => {
+    ge.buildAndGetErrors(repoDir, tscPath, topGithubRepos, skipLibCheck)
       .then(r => { process.send!(r), process.exit(); });
 });
