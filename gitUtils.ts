873bb0cd5d81da78664b96300cdabb698eb819b8
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2022-06-16 10:26:42 -0700

    remove obvious-ish type annotation

--- gitUtils.ts
+++ gitUtils.ts
@@ -18,7 +18,7 @@ const repoProperties = {
     repo: "typescript",
 };
 
-export async function getPopularTypeScriptRepos(count = 100, repoStartIndex: number = 0, cachePath?: string): Promise<readonly Repo[]> {
+export async function getPopularTypeScriptRepos(count = 100, repoStartIndex = 0, cachePath?: string): Promise<readonly Repo[]> {
     const cacheEncoding = { encoding: "utf-8" } as const;
 
     if (cachePath && await utils.exists(cachePath)) {
