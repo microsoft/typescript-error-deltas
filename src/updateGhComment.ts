5a2fe8d616fa18cfb22708c357d8df2d8a700d8c
Andrew Casey <amcasey@users.noreply.github.com>
2022-08-16 16:04:43 -0700

    Improve the output of the Top100 suite (#60)
    * Eliminate impossible status
    
    * Distinguish between top-repos and user tests in results comments
    
    * Drop redundant run url
    
    * Paginate UT results

--- src/updateGhComment.ts
+++ src/updateGhComment.ts
@@ -7,11 +7,12 @@ import pu = require("./packageUtils");
 const { argv } = process;
 
 if (argv.length !== 8) {
-    console.error(`Usage: ${path.basename(argv[0])} ${path.basename(argv[1])} <user_to_tag> <pr_number> <comment_number> <result_dir_path> <log_uri> <post_result>`);
+    console.error(`Usage: ${path.basename(argv[0])} ${path.basename(argv[1])} <user_to_tag> <pr_number> <comment_number> <is_top_repos_run> <result_dir_path> <post_result>`);
     process.exit(-1);
 }
 
-const [, , userToTag, prNumber, commentNumber, resultDirPath, logUri, post] = argv;
+const [, , userToTag, prNumber, commentNumber, isTop, resultDirPath, post] = argv;
+const isTopReposRun = isTop.toLowerCase() === "true";
 const postResult = post.toLowerCase() === "true";
 
 const metadataFilePaths = pu.glob(resultDirPath, `**/${metadataFileName}`);
@@ -20,7 +21,6 @@ let newTscResolvedVersion: string | undefined;
 let oldTscResolvedVersion: string | undefined;
 
 let somethingChanged = false;
-let somethingBroke = false;
 let infrastructureFailed = false;
 
 for (const path of metadataFilePaths) {
@@ -37,9 +37,6 @@ for (const path of metadataFilePaths) {
             case "Detected interesting changes":
                 somethingChanged = true;
                 break;
-            case "Detected project-graph error":
-                somethingBroke = false;
-                break;
             default:
                 infrastructureFailed = true;
                 break;
@@ -49,13 +46,10 @@ for (const path of metadataFilePaths) {
 
 let summary: string;
 if (infrastructureFailed) {
-    summary = `Unfortunately, something went wrong, but it probably wasn't caused by your change`;
-}
-else if (somethingBroke) {
-    summary = `It looks like this change broke something in a way that prevented tsc from running properly`;
+    summary = `Unfortunately, something went wrong, but it probably wasn't caused by your change.`;
 }
 else if (somethingChanged) {
-    summary = `Something interesting changed - please have a look`;
+    summary = `Something interesting changed - please have a look.`;
 }
 else {
     summary = `Everything looks good!`;
@@ -64,22 +58,31 @@ else {
 const resultPaths = pu.glob(resultDirPath, `**/*.${resultFileNameSuffix}`).sort((a, b) => path.basename(a).localeCompare(path.basename(b)));
 const outputs = resultPaths.map(p => fs.readFileSync(p, { encoding: "utf-8" }));
 
-// TODO: this should probably be paginated
-let body = `@${userToTag} Here are the results of running the user test suite comparing \`${oldTscResolvedVersion}\` and \`${newTscResolvedVersion}\`:
+const suiteDescription = isTopReposRun ? "top-repos" : "user test";
+let header = `@${userToTag} Here are the results of running the ${suiteDescription} suite comparing \`${oldTscResolvedVersion}\` and \`${newTscResolvedVersion}\`:
 
 ${summary}`;
 
-if (outputs.length) {
-    body += `
-
-<details>
-<summary>Details</summary>
-
-${outputs.join("")}
-</details>
-`;
+if (!outputs.length) {
+    git.createComment(+prNumber, +commentNumber, postResult, [header]);
 }
+else {
+    const openDetails = `\n\n<details>\n<summary>Details</summary>\n\n`;
+    const closeDetails = `\n</details>`;
 
-body += `\n\n[Run logs](${logUri})`;
+    // GH caps the maximum body length, so paginate if necessary
+    const bodyChunks: string[] = [];
+    let chunk = header + openDetails;
+    for (const output of outputs) {
+        if (chunk.length + output.length + closeDetails.length> 65536) {
+            chunk += closeDetails;
+            bodyChunks.push(chunk);
+            chunk = `@${userToTag} Here are some more interesting changes from running the ${suiteDescription} suite${openDetails}`;
+        }
+        chunk += output;
+    }
+    chunk += closeDetails;
+    bodyChunks.push(chunk);
 
-git.createComment(+prNumber, +commentNumber, postResult, body);
+    git.createComment(+prNumber, +commentNumber, postResult, bodyChunks);
+}
