6376334a2ba1da834fed10990b60a409cd5ab964
Andrew Casey <andrew.casey@microsoft.com>
2022-09-16 21:23:05 +0000

    Introduce a seed for the PRNG
    This will make it possible to repeat the randomly chosen LS operations, but will not prevent changes in the TS or the repos under test from causing the output to differ.  Pipeline timeouts may also vary from run to run.

--- src/checkGithubRepos.ts
+++ src/checkGithubRepos.ts
@@ -4,11 +4,11 @@ import { mainAsync, reportError, TsEntrypoint } from "./main";
 const { argv } = process;
 
 if (argv.length !== 10) {
-    console.error(`Usage: ${path.basename(argv[0])} ${path.basename(argv[1])} <ts_entrypoint> <old_ts_npm_version> <new_ts_npm_version> <repo_list_path> <worker_count> <worker_number> <result_dir_name> <diagnostic_output>`);
+    console.error(`Usage: ${path.basename(argv[0])} ${path.basename(argv[1])} <ts_entrypoint> <old_ts_npm_version> <new_ts_npm_version> <repo_list_path> <worker_count> <worker_number> <result_dir_name> <diagnostic_output> <prng_seed>`);
     process.exit(-1);
 }
 
-const [,, entrypoint, oldTsNpmVersion, newTsNpmVersion, repoListPath, workerCount, workerNumber, resultDirName, diagnosticOutput] = argv;
+const [,, entrypoint, oldTsNpmVersion, newTsNpmVersion, repoListPath, workerCount, workerNumber, resultDirName, diagnosticOutput, prngSeed] = argv;
 
 mainAsync({
     testType: "github",
@@ -22,6 +22,7 @@ mainAsync({
     oldTsNpmVersion,
     newTsNpmVersion,
     resultDirName,
+    prngSeed,
 }).catch(err => {
     reportError(err, "Unhandled exception");
     process.exit(1);
--- src/checkUserTestRepos.ts
+++ src/checkUserTestRepos.ts
@@ -3,12 +3,12 @@ import { mainAsync, reportError, TsEntrypoint } from "./main";
 
 const { argv } = process;
 
-if (argv.length !== 12) {
-    console.error(`Usage: ${path.basename(argv[0])} ${path.basename(argv[1])} <ts_entrypoint> <old_ts_repo_url> <old_head_ref> <pr_number> <is_top_repos> <repo_list_path> <worker_count> <worker_number> <result_dir_name> <diagnostic_output>`);
+if (argv.length !== 13) {
+    console.error(`Usage: ${path.basename(argv[0])} ${path.basename(argv[1])} <ts_entrypoint> <old_ts_repo_url> <old_head_ref> <pr_number> <is_top_repos> <repo_list_path> <worker_count> <worker_number> <result_dir_name> <diagnostic_output> <prng_seed>`);
     process.exit(-1);
 }
 
-const [,, entrypoint, oldTsRepoUrl, oldHeadRef, prNumber, buildWithNewWhenOldFails, repoListPath, workerCount, workerNumber, resultDirName, diagnosticOutput] = argv;
+const [,, entrypoint, oldTsRepoUrl, oldHeadRef, prNumber, buildWithNewWhenOldFails, repoListPath, workerCount, workerNumber, resultDirName, diagnosticOutput, prngSeed] = argv;
 
 mainAsync({
     testType: "user",
@@ -23,6 +23,7 @@ mainAsync({
     workerNumber: +workerNumber,
     resultDirName,
     diagnosticOutput: diagnosticOutput.toLowerCase() === "true",
+    prngSeed,
 }).catch(err => {
     reportError(err, "Unhandled exception");
     process.exit(1);
