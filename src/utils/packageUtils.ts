5e085a54931281c4dde2265dd16cc8d7dcb94715
Andrew Casey <andrew.casey@microsoft.com>
2022-10-11 17:30:28 +0000

    Extract helper for monorepo package ordering

--- src/utils/packageUtils.ts
+++ src/utils/packageUtils.ts
@@ -37,50 +37,54 @@ export async function getMonorepoOrder(repoDir: string): Promise<readonly string
         const lernaDir = path.dirname(lernaFile);
         if (await exists(path.join(lernaDir, "packages"))) {
             const pkgPaths = glob(path.join(lernaDir, "packages"), "**/package.json");
-            const pkgs = await Promise.all(pkgPaths.map(async pkgPath => {
-                const contents = await fs.promises.readFile(pkgPath, { encoding: "utf-8" });
-                const pkg: Package = json5.parse(contents);
-                pkg.meta_dir = path.dirname(pkgPath);
-                pkg.meta_state = "unvisited";
-                return pkg;
-            }));
-            const pkgMap: Record<string, Package | undefined> = {};
-            for (const pkg of pkgs) {
-                pkgMap[pkg.name] = pkg;
-            }
-
-            while (true) {
-                const pkg = pkgs.find(p => p.meta_state === "unvisited");
-                if (!pkg) break;
-                visit(pkg);
-            }
-
-            function visit(pkg: Package): void {
-                // "visiting" indicates a cycle, which lerna allows
-                if (pkg.meta_state !== "unvisited") return;
-
-                pkg.meta_state = "visiting";
-
-                for (const dep in pkg.dependencies) {
-                    const depPkg = pkgMap[dep];
-                    if (depPkg) visit(depPkg);
-                }
-
-                for (const dep in pkg.devDependencies) {
-                    const depPkg = pkgMap[dep];
-                    if (depPkg) visit(depPkg);
-                }
-
-                for (const dep in pkg.peerDependencies) {
-                    const depPkg = pkgMap[dep];
-                    if (depPkg) visit(depPkg);
-                }
-
-                pkg.meta_state = "visited";
-                monorepoOrder.push(pkg.meta_dir);
-            }
+            await getMonorepoOrderWorker(pkgPaths, monorepoOrder);
         }
     }
 
     return monorepoOrder;
 }
+async function getMonorepoOrderWorker(pkgPaths: string[], monorepoOrder: string[]) {
+    const pkgs = await Promise.all(pkgPaths.map(async (pkgPath) => {
+        const contents = await fs.promises.readFile(pkgPath, { encoding: "utf-8" });
+        const pkg: Package = json5.parse(contents);
+        pkg.meta_dir = path.dirname(pkgPath);
+        pkg.meta_state = "unvisited";
+        return pkg;
+    }));
+    const pkgMap: Record<string, Package | undefined> = {};
+    for (const pkg of pkgs) {
+        pkgMap[pkg.name] = pkg;
+    }
+
+    while (true) {
+        const pkg = pkgs.find(p => p.meta_state === "unvisited");
+        if (!pkg) break;
+        visit(pkg);
+    }
+
+    function visit(pkg: Package): void {
+        // "visiting" indicates a cycle, which some monorepo systems (e.g. lerna) allow
+        if (pkg.meta_state !== "unvisited") return;
+
+        pkg.meta_state = "visiting";
+
+        for (const dep in pkg.dependencies) {
+            const depPkg = pkgMap[dep];
+            if (depPkg) visit(depPkg);
+        }
+
+        for (const dep in pkg.devDependencies) {
+            const depPkg = pkgMap[dep];
+            if (depPkg) visit(depPkg);
+        }
+
+        for (const dep in pkg.peerDependencies) {
+            const depPkg = pkgMap[dep];
+            if (depPkg) visit(depPkg);
+        }
+
+        pkg.meta_state = "visited";
+        monorepoOrder.push(pkg.meta_dir);
+    }
+}
+
