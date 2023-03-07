8444a12dc2fb1a95bb3e196dd3c3a7624448f315
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-03-06 21:34:34 -0800

    Add test262 repo (#102)

---
+++ userTests/test262/build.sh
@@ -0,0 +1,10 @@
+rm -rf test262
+git clone --depth 1 https://github.com/tc39/test262.git test262
+START=$(pwd)
+cd $TS
+npm link
+cd $START/test262
+npm i
+npm link typescript
+cp $START/tsconfig.json .
+npx tsc
