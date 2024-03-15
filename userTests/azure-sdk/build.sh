dc586f9294877b46f0ec431010326c4930a83451
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-03-14 21:27:09 -0700

    Make rush work

--- userTests/azure-sdk/build.sh
+++ userTests/azure-sdk/build.sh
@@ -1,16 +1,18 @@
-npm install -g @microsoft/rush
+set -x
+command -v rush &> /dev/null || npm install -g @microsoft/rush
 rm -rf azure-sdk
 git clone --depth 1 https://github.com/Azure/azure-sdk-for-js.git azure-sdk
 cd azure-sdk
 START=$(pwd)
 rush update
-cd sdk/core/core-http
+cd sdk/core/core-util
 # Sync up all TS versions used internally so they're all linked from a known location
-rush add -p "typescript@3.5.1" --dev -m
+LOCAL_TS=$(node -e 'console.log(JSON.parse(fs.readFileSync("node_modules/typescript/package.json", "utf8")).version)')
+rush add -p "typescript@$LOCAL_TS" --dev -m
 # -nervous laugh-
 # Relink installed TSes to built TS
-cd $START/common/temp/node_modules/.pnpm/typescript@3.5.1/node_modules
+cd $START/common/temp/node_modules/.pnpm/typescript@$LOCAL_TS/node_modules
 rm -rf typescript
 ln -s $TS ./typescript
 cd $START
-rush rebuild --parallelism 1
+rush rebuild
