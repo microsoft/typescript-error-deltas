c54c66435886a14ee4a9a79bfb02feaa1072b0bd
Armando Aguirre <armando.aguirre@microsoft.com>
2024-05-01 14:20:05 -0700

    Add unit test to mainAsync (#151)
    * Added main unit test
    
    * Fixed test failure
    
    * Fixed path and asyncDispose
    
    * Removed asyncDispose polyfill
    
    * asyncDispose polyfill is back
    
    ---------
    
    Co-authored-by: Armando Aguirre Sepulveda <araguir@microsoft.com>

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,11 +1,117 @@
-/// <reference types="jest" />
-import { getTscRepoResult, downloadTsRepoAsync } from '../src/main'
+import { getTscRepoResult, downloadTsRepoAsync, mainAsync } from '../src/main'
 import { execSync } from "child_process"
-import { existsSync, mkdirSync } from "fs"
 import path = require("path")
 import { createCopyingOverlayFS } from '../src/utils/overlayFS'
+
+jest.mock('random-seed', () => ({
+    create: () => {
+        return {
+            random: () => 1,
+            seed: () => { },
+            string: () => ''
+        };
+    },
+}));
+jest.mock("../src/utils/packageUtils", () => ({
+    exists: jest.fn().mockResolvedValue(true),
+    getMonorepoOrder: jest.fn().mockResolvedValue([
+        "./dirA/package.json",
+        "./dirB/dirC/package.json",
+        "./dirD/DirE/dirF/package.json"
+    ])
+}));
+jest.mock("../src/utils/execUtils", () => ({
+    spawnWithTimeoutAsync: jest.fn((cwd: string, command: string, args: readonly string[], timeoutMs: number, env?: {}) => {
+        if (command === 'npm') {
+            // Return nothing so that npm install appears successfull.
+            return {};
+        }
+
+        return {
+            stdout: JSON.stringify({
+                "request_seq": "123",
+                "command": "cursedCommand",
+                "message": "Some error. Could not do something. \nMaybe a Debug fail.\n    at a (/mnt/vss/_work/1/s/typescript-1.1.1/lib/typescript.js:1:1)\n    at b (/mnt/vss/_work/1/s/typescript-1.1.1/lib/typescript.js:2:2)\n    at c (/mnt/vss/_work/1/s/typescript-1.1.1/lib/typescript.js:3:3)\n    at d (/mnt/vss/_work/1/s/typescript-1.1.1/lib/typescript.js:4:4)\n    at e (/mnt/vss/_work/1/s/typescript-1.1.1/lib/typescript.js:5:5)"
+            }),
+            stderr: '',
+            code: 5,
+            signal: null,
+
+        }
+    }),
+    execAsync: async (cwd: string, command: string) => {
+        if (command.startsWith('npm pack typescript@latest')) {
+            return ' typescript-0.0.0.tgz';
+        } else if (command.startsWith('npm pack typescript@next')) {
+            return ' typescript-1.1.1.tgz';
+        } else if (command.startsWith('git rev-parse')) {
+            return '57b462387e88aa7e363af0daf867a5dc1e83a935';
+        }
+
+        return '';
+    }
+
+}));
+jest.mock('fs', () => ({
+    promises: {
+        writeFile: jest.fn(),
+        copyFile: jest.fn(),
+        rename: jest.fn().mockResolvedValue(undefined),
+    },
+    readFileSync: (path: string) => {
+        if (path.endsWith("replay.txt")) {
+            return '{\"rootDirPlaceholder\":\"@PROJECT_ROOT@\",\"serverArgs\":[\"--disableAutomaticTypingAcquisition\"]}\r\n{\"seq\":1,\"type\":\"request\",\"command\":\"configure\",\"arguments\":{\"preferences\":{\"disableLineTextInReferences\":true,\"includePackageJsonAutoImports\":\"auto\",\"includeCompletionsForImportStatements\":true,\"includeCompletionsWithSnippetText\":true,\"includeAutomaticOptionalChainCompletions\":true,\"includeCompletionsWithInsertText\":true,\"includeCompletionsWithClassMemberSnippets\":true,\"allowIncompleteCompletions\":true,\"includeCompletionsForModuleExports\":false},\"watchOptions\":{\"excludeDirectories\":[\"**/node_modules\"]}}}\r\n{\"seq\":2,\"type\":\"request\",\"command\":\"updateOpen\",\"arguments\":{\"changedFiles\":[],\"closedFiles\":[],\"openFiles\":[{\"file\":\"@PROJECT_ROOT@/sample_repoName.config.js\",\"projectRootPath\":\"@PROJECT_ROOT@\"}]}}\r\n{\"seq\":3,\"type\":\"request\",\"command\":\"cursedCommand\",\"arguments\":{\"file\":\"@PROJECT_ROOT@/src/sampleTsFile.ts\",\"line\":1,\"offset\":1,\"includeExternalModuleExports\":false,\"triggerKind\":1}}';;
+
+        } else if (path.endsWith('repos.json')) {
+            return JSON.stringify([{
+                "url": "https://github.com/MockRepoOwner/MockRepoName",
+                "name": "MockRepoName",
+                "owner": "MockRepoOwner"
+            }]);
+        }
+    }
+}));
+jest.mock('../src/utils/installPackages', () => {
+    const actualIp = jest.requireActual('../src/utils/installPackages')
+    const npmCommand = {
+        tool: 'npm',
+        arguments: [
+            '--prefer-offline',
+            '--no-audit',
+            '--no-progress',
+            '--legacy-peer-deps',
+            '--ignore-scripts',
+            '-q',
+        ]
+    };
+
+    return {
+        InstallTool: actualIp.InstallTool,
+        installPackages: async () => {
+            return [
+                {
+                    ...npmCommand,
+                    directory: './dirA',
+                    prettyDirectory: 'dirA',
+                },
+                {
+                    ...npmCommand,
+                    directory: './dirB/dirC',
+                    prettyDirectory: 'dirB/dirC',
+                },
+                {
+                    ...npmCommand,
+                    directory: './dirD/dirE/dirF',
+                    prettyDirectory: 'dirD/dirE/dirF',
+                }
+            ]
+        }
+    }
+});
+
 describe("main", () => {
-    jest.setTimeout(10 * 60 * 1000)
+    jest.setTimeout(10 * 60 * 1000);
+
     xit("build-only correctly caches", async () => {
         const { status, summary } = await getTscRepoResult(
             {
@@ -22,14 +128,43 @@ describe("main", () => {
         expect(summary).toBeDefined()
         expect(summary!.startsWith(`# [TypeScript-Node-Starter](https://github.com/Microsoft/TypeScript-Node-Starter.git)`)).toBeTruthy()
         expect(summary!.includes("- \`error TS2496: The 'arguments' object cannot be referenced in an arrow function in ES3 and ES5. Consider using a standard function expression.\`")).toBeTruthy()
-    })
+    });
+
     it("downloads from a branch", async () => {
-        if (!existsSync("./testDownloads/main")) {
-            mkdirSync("./testDownloads/main", { recursive: true });
+        const actualFs = jest.requireActual('fs');
+
+        if (!actualFs.existsSync("./testDownloads/main")) {
+            actualFs.mkdirSync("./testDownloads/main", { recursive: true });
         }
-        else if (existsSync("./testDownloads/main/typescript-test-fake-error")) {
+        else if (actualFs.existsSync("./testDownloads/main/typescript-test-fake-error")) {
             execSync("cd ./testDownloads/main/typescript-test-fake-error && git restore . && cd ..")
         }
         await downloadTsRepoAsync('./testDownloads/main', 'https://github.com/sandersn/typescript', 'test-fake-error', 'tsc')
-    })
+    });
+
+    it("outputs server errors", async () => {
+        const mockedFs = require('fs');
+
+        await mainAsync({
+            testType: "github",
+            tmpfs: false,
+            entrypoint: 'tsserver',
+            diagnosticOutput: false,
+            buildWithNewWhenOldFails: false,
+            repoListPath: "./artifacts/repos.json",
+            workerCount: 1,
+            workerNumber: 1,
+            oldTsNpmVersion: 'latest',
+            newTsNpmVersion: 'next',
+            resultDirName: './artifacts',
+            prngSeed: 'testSeed',
+        });
+
+        // Remove all references to the base path so that snapshot pass successfully.
+        mockedFs.promises.writeFile.mock.calls.forEach((e: [string, string]) => {
+            e[0] = e[0].replace(process.cwd(), "<BASE_PATH>");
+        });
+
+        expect(mockedFs.promises.writeFile).toMatchSnapshot();
+    });
 })
