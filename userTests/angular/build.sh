8b1b07d12f77685c5abbbdd9e75c5a141cec9ca2
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-09-26 16:56:51 -0700

    Add angular user test (#170)

---
+++ userTests/angular/build.sh
@@ -0,0 +1,13 @@
+npm i -g yarn --force
+rm -rf angular
+git clone --depth 1 https://github.com/angular/angular angular
+START=$(pwd)
+cd $TS
+rm ~/.config/yarn/link/typescript
+yarn link
+
+cd $START/angular
+yarn link typescript
+yarn install --ignore-scripts
+
+yarn tsc -p ./packages/tsconfig.json
