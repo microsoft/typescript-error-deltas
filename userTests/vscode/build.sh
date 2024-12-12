568680081e93bd93875ef46fd134a45e80b65bcf
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-12-12 11:52:52 -0800

    Fix vscode

--- userTests/vscode/build.sh
+++ userTests/vscode/build.sh
@@ -1,22 +1,15 @@
-# TODO: building vs code relies on python 3 as well
-# although it might be part of node-gyp's postinstall script and could be skipped safely
-apt-get update
-apt-get install libsecret-1-dev libx11-dev libxkbfile-dev -y
-npm i -g yarn --force
 rm -rf vscode
 git clone --depth 1 https://github.com/microsoft/vscode.git vscode
 START=$(pwd)
 cd $TS
-rm ~/.config/yarn/link/typescript
-yarn link
+npm link
 
 cd $START/vscode/build
-yarn link typescript
+npm link typescript
 cd $START/vscode/extensions
-yarn add rimraf
-yarn link typescript
+npm add rimraf
+npm link typescript
 cd $START/vscode
-yarn link typescript
-yarn
-yarn compile
-
+npm link typescript
+npm install
+npm run compile
