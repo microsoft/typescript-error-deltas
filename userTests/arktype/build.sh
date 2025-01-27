da4aa02ac2094ad7cdb1bf01175ee35e27ada88c
David Blass <david@arktype.io>
2025-01-27 15:09:32 -0500

    switch to typescript-eslint strategy

--- userTests/arktype/build.sh
+++ userTests/arktype/build.sh
@@ -1,12 +1,8 @@
 npm i -g pnpm
 rm -rf arktype
 git clone --depth 1 https://github.com/arktypeio/arktype.git arktype
+cd arktype
 
-START=$(pwd)
-cd $TS
-pnpm link
-cd $START/arktype
-
-pnpm link typescript
+npx json -I -f package.json -e "this.resolutions.typescript = 'file:$TS'"
 pnpm i
-pnpm build
+pnpm tsc
