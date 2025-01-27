ba6ad28f95feaccbadc6836386cf991f2afbbaf5
David Blass <david@arktype.io>
2025-01-27 13:41:39 -0500

    fix linking

--- userTests/arktype/build.sh
+++ userTests/arktype/build.sh
@@ -1,7 +1,12 @@
 npm i -g pnpm
 rm -rf arktype
 git clone --depth 1 https://github.com/arktypeio/arktype.git arktype
-cd arktype
 
+START=$(pwd)
+cd $TS
+pnpm link
+cd $START/arktype
+
+pnpm link typescript
 pnpm i
 pnpm build
