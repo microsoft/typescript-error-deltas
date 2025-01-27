52f3bab11a62ce91e733c59b6ed1b1ba29dc2672
David Blass <david@arktype.io>
2025-01-27 16:18:03 -0500

    Add ArkType to userTests (#185)

---
+++ userTests/arktype/build.sh
@@ -0,0 +1,10 @@
+set -x
+
+npm i -g pnpm
+rm -rf arktype
+git clone --depth 1 https://github.com/arktypeio/arktype.git arktype
+cd arktype
+
+npx json -I -f package.json -e "this.resolutions = { ...this.resolutions, typescript: 'file:$TS' } "
+pnpm i
+pnpm tsc
